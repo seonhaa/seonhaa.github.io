7b15832b3aacc26273dff7ffb78cc4b3e0d71e07
SeonHaa <115872089+seonhaa@users.noreply.github.com>
2025-12-22 22:46:28 +0900

    Youtube-mainpage 추가

---
+++ Youtube_Clone/MainPage/script.js
@@ -0,0 +1,9 @@
+function toggleMore() {
+  const moreSearch = document.querySelector("#moreSearch");
+  moreSearch.classList.toggle("active");
+}
+
+function toggleHidden() {
+  const layout = document.querySelector(".layout");
+  layout.classList.toggle("collapsed");
+}
