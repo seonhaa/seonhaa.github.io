cfaa29809209a6c5f51775393256375db76a2b11
황선하 <jhh8210@naver.com>
2026-08-12 11:07:31 +0900

    code 수정

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { React } from "react";
 import { useState } from "react";
 
 function App() {
-  const [num1, setNum1] = useState("");
-  const [num2, setNum2] = useState("");
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
   const [result, setResult] = useState("");
 
   return (
@@ -18,16 +18,19 @@ function App() {
       <div>
         <input
           value={num1}
-          onChange={(e) => setNum1(Number(e.target.value))}
+          onChange={(e) => setNum1(e.target.value)}
           placeholder="숫자1 입력"
         />{" "}
         <input
           value={num2}
-          onChange={(e) => setNum2(Number(e.target.value))}
+          onChange={(e) => setNum2(e.target.value)}
           placeholder="숫자2 입력"
         />
         {/* 버튼 클릭 시 연산 진행 */}
-        <button onClick={() => setResult(num1 + num2)}> + </button>
+        <button onClick={() => setResult(Number(num1) + Number(num2))}>
+          {" "}
+          +{" "}
+        </button>
         <button onClick={() => setResult(num1 - num2)}> - </button>
         <button onClick={() => setResult(num1 * num2)}> * </button>
         <button onClick={() => setResult(num1 / num2)}> / </button>
